5defc0745129710bd3f1c7a158a96e5a0ca06564
marioluisgl <ml.garcia.lugones@gmail.com>
2020-07-20 21:13:00 -0400

    frontend eztructure and files

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { NotFound404Component } from './shared/not-found404/not-found404.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { GraficsComponent } from './pages/grafics/grafics.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NotFound404Component,
+    DashboardComponent,
+    ProgressComponent,
+    GraficsComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule,
